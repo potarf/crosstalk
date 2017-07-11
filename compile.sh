7a279a75e899a0142bc96b6e6952e31a5aef4bab
Master Goldfish <bosonbaas@gmail.com>
2017-07-11 14:19:34 +0200

    Made the compilation method more user friendly

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-javac -d ./build/classes -cp source ./source/Main.java
+mkdir -p build/classes                                                           
+mkdir -p dist                                                                    
+javac -d ./build/classes -cp source ./source/Main.java                           
 jar cvfe dist/crosstalk.jar Main -C build/classes .
